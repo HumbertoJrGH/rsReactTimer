ce39f79654a4ed5af273670cba343af307146ca7
HumbertJrGH <dev.humbertojr@gmail.com>
2022-09-30 07:41:31 -0300

    atualização

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,16 +8,25 @@ import {
     TaskInput,
     TimeMarkup
 } from "./styles";
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export default function Home() {
+
     return (
         <HomeContainer>
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" type="text" placeholder="Dê um nome para sua tarefa" />
+                    <TaskInput id="task" list="taskDataList" type="text" placeholder="Dê um nome para sua tarefa" />
+                    <datalist id="taskDataList">
+                        <option value="projeto 1" />
+                        <option value="projeto 2" />
+                        <option value="projeto 3" />
+                        <option value="projeto 4" />
+                    </datalist>
+
                     <label htmlFor="duration">durante</label>
-                    <DurationInput id="duration" type="number" placeholder="00" />
+                    <DurationInput id="duration" type="number" placeholder="00" step={5} min={5} max={60} />
                     <label htmlFor="duration">minutos</label>
                 </FormContainer>
 
--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -50,11 +50,24 @@ const BaseInput = styled.input`
     font-weight: bold;
     font-size: inherit;
     padding: 0 0.5rem;
-    color: ${props => props.theme['gray-100']}
+    color: ${props => props.theme['gray-100']};
+
+    &:focus {
+        box-shadow: none;
+        border-color: ${props => props.theme['green-500']}
+    }
+
+    &::placeholder {
+        color: ${props => props.theme['gray-100']}
+    }
 `
 
 export const TaskInput = styled(BaseInput)`
     flex: 1;
+
+    &::-webkit-calendar-picker-indicator {
+        display: none !important;
+    }
 `
 
 export const DurationInput = styled(BaseInput)`
