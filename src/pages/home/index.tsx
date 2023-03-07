f8b7f27fa17aa1fb794a1cd4667117f78d208557
HumbertJrGH <dev.humbertojr@gmail.com>
2023-03-06 23:35:55 -0300

    atualização

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { Play } from "phosphor-react"
+import { useEffect, useState } from "react"
+import { Play } from "phosphor-react";
 import {
     CountdownButton,
     CountdownContainer,
@@ -8,28 +8,90 @@ import {
     HomeContainer,
     TaskInput,
     TimeMarkup
-<<<<<<< HEAD
 } from "./styles";
+
 import { useForm } from 'react-hook-form'
-=======
-} from "./styles"
-import axios, { AxiosError, AxiosResponse } from 'axios'
->>>>>>> 398decec67d3db3ebc72d61f3f2986a07618c1bb
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { differenceInSeconds } from "date-fns";
+
+const newCycleSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    duration: zod.number().min(5).max(60, 'O valor selecionado precisa ser de até 60 minutos')
+})
+
+type ICycleForm = zod.infer<typeof newCycleSchema>
+
+interface Cycle {
+    id: string
+    task: string
+    duration: number
+    startDate: Date
+}
 
 export default function Home() {
-    const [task, setTask] = useState('')
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [active, setActive] = useState<string | null>(null)
+    const [past, setPast] = useState(0)
+
+    const { register, handleSubmit, watch, reset } = useForm<ICycleForm>({
+        resolver: zodResolver(newCycleSchema),
+        defaultValues: {
+            task: '',
+            duration: 5
+        }
+    })
+
+    function handleCreateCycle(data: ICycleForm) {
+        const id = String(new Date().getTime())
+        const newCycle: Cycle = {
+            id: id,
+            task: data.task,
+            duration: data.duration,
+            startDate: new Date
+        }
+
+        setCycles(prev => [...prev, newCycle])
+        setActive(id)
+        setPast(0)
+
+        reset()
+    }
+
+    const activeCycle = cycles.find(cycle => cycle.id == active)
+
+    const totalSec = activeCycle ? activeCycle.duration * 60 : 0
+    const currentSec = activeCycle ? totalSec - past : 0
+
+    const minutes = Math.floor(currentSec / 60)
+    const seconds = currentSec % 60
+
+    const minute = String(minutes).padStart(2, '0')
+    const second = String(seconds).padStart(2, '0')
+
+    const task = watch('task')
+
+    useEffect(() => {
+        if (activeCycle) {
+            const interval = setInterval(() => {
+                setPast(differenceInSeconds(new Date(), activeCycle.startDate))
+            }, 1000)
+
+            return () => { clearInterval(interval) }
+        }
+
+    }, [activeCycle])
 
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
                         list="taskDataList"
-                        type="text"
                         placeholder="Dê um nome para sua tarefa"
-                        onChange={e => setTask(e.target.value)}
+                        {...register('task')}
                     />
                     <datalist id="taskDataList">
                         <option value="projeto 1" />
@@ -39,19 +101,30 @@ export default function Home() {
                     </datalist>
 
                     <label htmlFor="duration">durante</label>
-                    <DurationInput id="duration" type="number" placeholder="00" step={5} min={5} max={60} />
+                    <DurationInput
+                        id="duration"
+                        type="number"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                        {...register('duration', { valueAsNumber: true })}
+                    />
                     <label htmlFor="duration">minutos</label>
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minute[0]}</span>
+                    <span>{minute[1]}</span>
                     <TimeMarkup>:</TimeMarkup>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{second[0]}</span>
+                    <span>{second[1]}</span>
                 </CountdownContainer>
 
-                <CountdownButton disabled={!task} type="submit">
+                <CountdownButton
+                    disabled={!task}
+                    type="submit"
+                >
                     <Play size={24} />
                     Começar
                 </CountdownButton>
